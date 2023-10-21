d9cb1e575d3193be79147625ff6f4c3adffd502c
chiara1911 <chiaracesari89@gmail.com>
2023-10-21 19:07:06 +0200

    add bomb function and lose game

--- js/script.js
+++ js/script.js
@@ -3,22 +3,29 @@
 
 
 // 1. Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
+
 // 2. Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. 
 // >> - Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
 // >> - In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+
+
+
 // >> - La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti(ovvero quando ha rivelato tutte le celle che non sono bombe).
 // >> - Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
 
 // 2. creare bottone per generare quadratini
+"use strict";
+
 const send = document.querySelector('input');
 const result = document.getElementById('result');
-
-let bombAr = createBombs();
+let bombAr = [];
 const numBomb = 16;
-console.log(bombAr)
+let squareNum;
+let difference;
 
 // richiamo funzionalità al bottone
 
@@ -26,15 +33,18 @@ send.addEventListener('click', function () {
     // dichiaro la costante della griglia intera
     const playground = document.getElementById('playground');
     // dichiaro quanti quadratini voglio in totale
-    const squareNum = parseInt(document.getElementById('difficult').value);
-    console.log(squareNum);
+    squareNum = parseInt(document.getElementById('difficult').value);
+    bombAr = createBombs();
+    console.log(bombAr);
+    difference = squareNum - numBomb;
+
     playground.innerHTML = '';
     //richiamo il ciclo per stampare i vari quadrati
     for (let i = 1; i <= squareNum; i++) {
         // genero quadratino chiamando la funzione
-        let square = miniSquare(i, squareNum);
+        let square1 = miniSquare(i, squareNum);
         // appendo il quadratino alla griglia
-        playground.append(square);
+        playground.append(square1);
     }
 
 });
@@ -43,41 +53,50 @@ function createBombs() {
     let bombs = [];
     let i = 0;
     //  generare 16 numeri random 
-    while (i < 16) {
+    while (i < numBomb) {
         //  generato un numero random
-        let bomb = parseInt(getRndInteger(1, 100));
+        let bomb = parseInt(getRndInteger(1, squareNum));
         // e l'aggiungo solo se non è già presente
         if (!bombs.includes(bomb)) {
-            bombs.push(bomb)
+            i++;
+            bombs.push(bomb);
         }
-        i++;
     }
-    return bombs
-
+    return bombs;
 }
+
+
 // fare una funzione per creare un quadratino
 
-function miniSquare(squareEl, squareNum) {
+function miniSquare(squareEl, allBox) {
 
-    squareWidth = Math.sqrt(squareNum);
-
+    let squareWidth = Math.sqrt(allBox);
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.width = `calc(100% / ${squareWidth})`;
     square.style.height = square.style.width;
     square.innerHTML = squareEl;
+
+
+
     square.addEventListener('click', function () {
-        if (squareEl !== bombs[i]) {
+        if (bombAr.includes(squareEl)) {
+            square.classList.add('bomb');
+            alert("hai perso!");
+
+        } else {
             square.classList.add('active');
             result.innerHTML = 'hai cliccato la casella' + ' ' + squareEl;
-        } else {
-            square.classList.add('bomb');
+            difference--;
+            if (difference === 0) {
+                alert('hai vinto');
+            }
 
         }
 
-
     })
     return square;
+
 };
 
 
